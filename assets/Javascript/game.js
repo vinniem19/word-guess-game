9df178a0e3d90a251c3a762dd94fcf9a454bb2e2
unknown <cuzvin2@gmail.com>
2019-02-27 00:41:36 -0500

    pseudocoded a lot and started a few functions

---
+++ assets/Javascript/game.js
@@ -0,0 +1,115 @@
+var houseItems = ["kitchen", "bed", "chair","door"];
+var playerGuess =[];
+var lettersGuessed=[];
+var numberWins=0;
+
+// Computer chooses word
+for (var i=0, i < houseItems.length; i++) {
+    let computerWord = (houseItems[i]);
+
+}
+
+
+
+// Hide the word, and show the underscores in its place
+
+
+
+// Player types letter, capture that event, and store it in the array of playerGuess
+
+
+
+
+// Compare players guess to string of letters in CHAIR
+if (computerWord === "chair" && playerGuess === "c" || "h" || "a" || "i" || "r") {
+
+        // write that letter in the appropriate space ;
+
+}
+
+// Compare player guess to string of letters in kitchen
+if (computerWord === "kitchen" && playerGuess === "k" || "i" ||"t" || "c" || "h" || "e" || "n") {
+
+        // write that letter in the appropriate space
+document.write (coputerWord)str.charAt(playerGuess);
+
+}
+
+
+//  Compare player guess to string of letters in bed
+if (computerWord) === "bed" && playerGuess === "b" || "e" || "d") {
+
+        // write that letter in the appropriate space
+
+}
+
+// Compare player guess to string of letters in door
+
+if (computerWord === "door" && playerGuess === "d" || "o" || "o" || "r") {
+
+    // write that letter in the appropriate space
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.getElementById("getkey").addEventListener("keyup", myFunction);
+
+function myFunction() {
+    var playerGuess = document.getElementById("getkey");
+    
+}
+
+
+
+for (var i = 0; i < houseItems.length; i++) {
+
+
+    if (playerGuess === .charAt[0,0] {
+
+
+
